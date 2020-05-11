e781bdea21a3842a4f24ad585e7bff5e6ff54322
IamWhan <guide_gang@hotmail.com>
2020-05-11 20:51:05 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = String2Int("1qaz2wsx3edc4rv");
+            Console.Write(result);
+        }
+
+        public static int String2Int(string val)
+        {
+            string result = string.Empty;
+            if(!string.IsNullOrEmpty(val))
+            {
+                char[] arr = val.ToCharArray();
+
+                foreach(var i in arr)
+                {
+                    switch ( i )
+                    {
+                        case '0' :
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                            result += i.ToString();
+                            break;
+                        default:
+                            result += "";
+                            break;
+
+                    }
+                     
+
+                }
+            }
+
+            return Convert.ToInt32(result);
+        }
+    }
+}
